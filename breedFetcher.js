6c4d65b585244f363c192ac80818ae5082c6bf57
Josh Kelly <jkelly85@alumni.uwo.ca>
2023-03-17 23:58:22 -0400

    breedFetch, index

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,28 @@
 const request = require('request');
-const query = process.argv[2];
-const requestedUrl = `https://api.thecatapi.com/v1/breeds/search?q=${query}`;
-request.get(requestedUrl, null, function(err, res, body) {
-  if (err) {
-    console.log('Error details: ' + err);
-    return;
-  }
-  if (res.statusCode === 200) {
-    if (res.body === '[]') {
-   
-      console.log("The requested breed is not in our database");
-      return;
 
-    }
+const fetchBreedDescription = function(breedName, cb) {
+
+  request.get(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, null, function(err, res, body) {
     const data = JSON.parse(res.body);
-    console.log(data[0].description);
-  }
-});
+
+    if (err) {
+      cb(err, null);
+      return;
+    }
+
+    if (res.statusCode === 200) {
+      if (res.body === '[]') {
+        
+        cb(`${breedName} is not in our database`);
+        return;
+        
+      }
+    }
+    
+    cb(null, data[0].description);
+
+  });
+
+};
+
+module.exports = { fetchBreedDescription };
